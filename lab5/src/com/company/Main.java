53f7e6afa8d513383ab3957fcf427cd078119245
lakiert <73653530+lakiert@users.noreply.github.com>
2021-11-03 11:08:38 +0100

    Update Main.java

--- lab5/src/com/company/Main.java
+++ lab5/src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
 import java.util.Scanner;
+import java.util.Random;
 import java.*;
 
 
 
 public class Main {
 
+
+    // zad1
 
     public static void showPrimes(int[] list)
     {
@@ -36,29 +39,87 @@ public class Main {
 
 
 
+    // zad2
+
+    public static void randomowe(int n)
+    {
+
+        Random r = new Random();
+        int ran = r.nextInt();
+        int[] tab = new int[n];
+        int minimalna = -50;
+        int maksymalna = 50;
+
+        for(int i=0; i<n; i++)
+        {
+            ran = r.nextInt(maksymalna - minimalna) + minimalna;
+            tab[i] = ran;
+        }
+
+        System.out.println(" ");
+
+        for(int i=0; i<n; i++)
+        {
+            System.out.println(tab[i]);
+        }
+
+        System.out.println(" ");
+
+        int najwiekszy = tab[0];
+        for(int i=0; i<n; i++)
+        {
+            if(tab[i] > najwiekszy) najwiekszy = tab[i];
+        }
+
+        System.out.println("element najwiekszy: " + najwiekszy);
+
+        int ile=0;
+        for(int i=0; i<n; i++)
+        {
+            if(tab[i] == najwiekszy) ile++;
+        }
+
+        System.out.println("wystepuje w tablicy " + ile + " razy");
+
+    }
+
+
+
+
 
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
 
+
+
         // zad1
+
+//        System.out.println("podaj ilosc liczb: ");
+//        int n = in.nextInt();
+//        int liczba;
+//        int[] lista = new int[n];
+//
+//        for(int i=0 ; i<n; i++)
+//        {
+//            System.out.println("podaj liczbe: ");
+//            liczba = in.nextInt();
+//            lista[i] = liczba;
+//        }
+//
+//        System.out.println(" ");
+//        System.out.println("liczby pierwsze: ");
+//
+//        showPrimes(lista);
+
+
+
+        // zad2
 
         System.out.println("podaj ilosc liczb: ");
         int n = in.nextInt();
-        int liczba;
-        int[] lista = new int[n];
 
-        for(int i=0 ; i<n; i++)
-        {
-            System.out.println("podaj liczbe: ");
-            liczba = in.nextInt();
-            lista[i] = liczba;
-        }
-
-        System.out.println(" ");
-        System.out.println("liczby pierwsze: ");
-
-        showPrimes(lista);
+        randomowe(n);
 
 
     }
